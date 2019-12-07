5a73679633a70ad7d998085b5ec5338cbb06f173
yu2519hn <1234568hH*>
2019-12-07 16:16:20 -0600

    initial commit of project practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,7 @@
-
-print('Hello 7abiby')
+# Import the request module from the url library
+import requests
+from PIL import Image
+import docx
+taco_url = 'https://taco-1150.herokuapp.com/random/?full_taco=true'
+taco_recipe = requests.get(taco_url).json()
+print(taco_recipe)
